8a2ef47963e2d1386a0e244648ec028d98fdd377
Rosendo E. Galang <regalang@gcg.gov>
2024-04-16 14:50:07 +0800

    Download pdf

--- views/dashboard/reports.view.php
+++ views/dashboard/reports.view.php
@@ -43,7 +43,7 @@
           </div>
         </div>
       </div>
-      <div class="table-responsive" style="max-height: 800px; overflow-y: auto;">
+      <div class="table-responsive" style="max-height: 800px; overflow-y: auto;" id="content">
         <table id="salesTable" class="report-table table">
           <thead>
             <tr>
@@ -60,7 +60,10 @@
       </div>
       <div class="row justify-content-center mb-2">
         <div class="col text-center">
-          <button class="download-btn btn btn-primary" data-report-type="sales" onclick="downloadPDF('salesReportContainer')">Download as PDF</button>
+          <button class="download-btn btn btn-primary" data-report-type="sales" onclick="downloadPDF('salesReportContainer')">Print as PDF</button>
+        </div>
+        <div class="col text-center">
+          <button class="download-btn btn btn-primary" data-report-type="sales" onclick="demoFromHTML()">Download as PDF</button>
         </div>
       </div>
     </div>
@@ -82,7 +85,7 @@
       </div>
     </div>
 
-    <div class="table-responsive" style="max-height: 800px; overflow-y: auto;">
+    <div class="table-responsive" style="max-height: 800px; overflow-y: auto;" id="content2">
       <table id="inventoryTable" class="report-table table mt-2">
         <thead>
           <tr>
@@ -100,34 +103,170 @@
     </div>
     <div class="row justify-content-center mb-2">
       <div class="col text-center">
-        <button class="download-btn btn btn-primary" data-report-type="inventory" onclick="downloadPDF('inventoryReportContainer')">Download as PDF</button>
+        <button class="download-btn btn btn-primary" data-report-type="inventory" onclick="downloadPDF('inventoryReportContainer')">Print as PDF</button>
+      </div>
+      <div class="col text-center">
+        <button class="download-btn btn btn-primary" data-report-type="inventory" onclick="demoFromHTML2()">Download as PDF</button>
       </div>
     </div>
   </div>
 
-  <div id="feedbackReportContainer" class="report-container">
-    <table id="feedbackTable" class="report-table table">
-      <thead>
-        <tr>
-          <th>Feedback ID</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Customer ID</th>
-        </tr>
-      </thead>
-      <tbody id="tableBodyFeedback"></tbody>
-    </table>
-    <div class="row justify-content-center mb-2">
-      <div class="col text-center">
-        <button class="download-btn btn btn-primary" data-report-type="feedback" onclick="downloadPDF('feedbackReportContainer')">Download as PDF</button>
+
+  <div id="content3">
+    <div id="feedbackReportContainer" class="report-container" >
+      <table id="feedbackTable" class="report-table table">
+        <thead>
+          <tr>
+            <th>Feedback ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Customer ID</th>
+          </tr>
+        </thead>
+        <tbody id="tableBodyFeedback"></tbody>
+      </table>
+      <div class="row justify-content-center mb-2">
+        <div class="col text-center">
+          <button class="download-btn btn btn-primary" data-report-type="feedback" onclick="downloadPDF('feedbackReportContainer')">Print as PDF</button>
+        </div>
+                <div class="col text-center">
+            <button class="download-btn btn btn-primary" data-report-type="sales" onclick="demoFromHTML3()">Download as PDF</button>
+          </div>
       </div>
     </div>
+
   </div>
+
 
   
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
   <script src="/Dashboard/js-dashboard/reports.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
+
+<script> 
+  function demoFromHTML() {
+      var pdf = new jsPDF('p', 'pt', 'letter');
+      // source can be HTML-formatted string, or a reference
+      // to an actual DOM element from which the text will be scraped.
+      source = $('#content')[0];
+
+      // we support special element handlers. Register them with jQuery-style 
+      // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+      // There is no support for any other type of selectors 
+      // (class, of compound) at this time.
+      specialElementHandlers = {
+          // element with id of "bypass" - jQuery style selector
+          '#bypassme': function (element, renderer) {
+              // true = "handled elsewhere, bypass text extraction"
+              return true
+          }
+      };
+      margins = {
+          top: 80,
+          bottom: 60,
+          left: 40,
+          width: 522
+      };
+      // all coords and widths are in jsPDF instance's declared units
+      // 'inches' in this case
+      pdf.fromHTML(
+          source, // HTML string or DOM elem ref.
+          margins.left, // x coord
+          margins.top, { // y coord
+              'width': margins.width, // max width of content on PDF
+              'elementHandlers': specialElementHandlers
+          },
+
+          function (dispose) {
+              // dispose: object with X, Y of the last line add to the PDF 
+              //          this allow the insertion of new lines after html
+              pdf.save('table_sales.pdf');
+          }, margins
+      );
+  }
+  function demoFromHTML2() {
+      var pdf = new jsPDF('p', 'pt', 'letter');
+      // source can be HTML-formatted string, or a reference
+      // to an actual DOM element from which the text will be scraped.
+      source = $('#content2')[0];
+
+      // we support special element handlers. Register them with jQuery-style 
+      // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+      // There is no support for any other type of selectors 
+      // (class, of compound) at this time.
+      specialElementHandlers = {
+          // element with id of "bypass" - jQuery style selector
+          '#bypassme': function (element, renderer) {
+              // true = "handled elsewhere, bypass text extraction"
+              return true
+          }
+      };
+      margins = {
+          top: 80,
+          bottom: 60,
+          left: 40,
+          width: 522
+      };
+      // all coords and widths are in jsPDF instance's declared units
+      // 'inches' in this case
+      pdf.fromHTML(
+          source, // HTML string or DOM elem ref.
+          margins.left, // x coord
+          margins.top, { // y coord
+              'width': margins.width, // max width of content on PDF
+              'elementHandlers': specialElementHandlers
+          },
+
+          function (dispose) {
+              // dispose: object with X, Y of the last line add to the PDF 
+              //          this allow the insertion of new lines after html
+              pdf.save('table_inventory.pdf');
+          }, margins
+      );
+  }
+
+  function demoFromHTML3() {
+      var pdf = new jsPDF('p', 'pt', 'letter');
+      // source can be HTML-formatted string, or a reference
+      // to an actual DOM element from which the text will be scraped.
+      source = $('#content3')[0];
+
+      // we support special element handlers. Register them with jQuery-style 
+      // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+      // There is no support for any other type of selectors 
+      // (class, of compound) at this time.
+      specialElementHandlers = {
+          // element with id of "bypass" - jQuery style selector
+          '#bypassme': function (element, renderer) {
+              // true = "handled elsewhere, bypass text extraction"
+              return true
+          }
+      };
+      margins = {
+          top: 80,
+          bottom: 60,
+          left: 40,
+          width: 522
+      };
+      // all coords and widths are in jsPDF instance's declared units
+      // 'inches' in this case
+      pdf.fromHTML(
+          source, // HTML string or DOM elem ref.
+          margins.left, // x coord
+          margins.top, { // y coord
+              'width': margins.width, // max width of content on PDF
+              'elementHandlers': specialElementHandlers
+          },
+
+          function (dispose) {
+              // dispose: object with X, Y of the last line add to the PDF 
+              //          this allow the insertion of new lines after html
+              pdf.save('table_report.pdf');
+          }, margins
+      );
+  }
+</script>
 
  
   
